b7b0caad19be3efe4fc3ec7d1a034b5c21630a29
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-06-07 19:27:28 +0300

    book fetch:created GetBooks function, that querys books from the API

--- src/App.js
+++ src/App.js
@@ -7,42 +7,41 @@ import {
   from,
 } from "@apollo/client";
 
-import {onError} from  '@apollo/client'
+import { onError } from "@apollo/client/link/error";
+import GetBooks from "./components/APIFetch";
 
-//catching graphql errors
-const errorLink = onError(({graphqlErrors,networkError}) =>{
-  if (graphqlErrors) {
-    graphqlErrors.map(({message, location, path}) =>{
-      alert(`Graphql erro ${message, location,path}`);
-    });
-  }
-  else if(networkError){
-    networkError.map(({message}) =>{
-      alert(`Network error ${message}`);
-    });
-  }
-})
+// Log any GraphQL errors or network error that occurred
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
-//connecting the Ello API 
+//connecting the Ello API
 const link = from([
   errorLink,
-  new HttpLink({uri:"https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql"})
+  new HttpLink({
+    uri: "https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql",
+  }),
 ]);
 
 //initializing apolloClient instance
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link:link
+  link: link,
 });
 
-
-
-
-
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ApolloProvider client={client}>
+      <GetBooks />
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- src/components/APIFetch.js
+++ src/components/APIFetch.js
@@ -1,1 +1,25 @@
-// import { ApolloClient, InMemoryCache, gql } from “apollo/client”;
+import {gql, useQuery} from '@apollo/client'
+import React,{useEffect} from 'react'
+export const LOAD_BOOKS =gql`
+query {
+    book{
+        pages{
+            content
+            tokens{
+                position
+                value
+            }
+        }
+    }
+}
+`
+function GetBooks(){
+    const {error, loading, data} =useQuery(LOAD_BOOKS);
+    useEffect(() =>{
+    console.log(data)
+    },[data]);
+
+    return <div>data</div>
+}
+
+export default GetBooks;
