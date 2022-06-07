e10567d7bf899fcef19685925a8711d670eb53d2
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-06-07 16:14:05 +0300

    initialized ApolloClient instance

--- src/App.js
+++ src/App.js
@@ -29,6 +29,12 @@ const link = from([
   new HttpLink({uri:"https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql"})
 ]);
 
+//initializing apolloClient instance
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link:link
+});
 
 
 
