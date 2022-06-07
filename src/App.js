e39f3a007db4d9038995933a0eed735a7302a083
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-06-07 16:04:49 +0300

    errors: added graphql and network error handling function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+
+import {onError} from  '@apollo/client'
+
+//catching graphql errors
+const errorLink = onError(({graphqlErrors,networkError}) =>{
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) =>{
+      alert(`Graphql erro ${message, location,path}`);
+    });
+  }
+  else if(networkError){
+    networkError.map(({message}) =>{
+      alert(`Network error ${message}`);
+    });
+  }
+})
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/APIFetch.js
@@ -0,0 +1,1 @@
+// import { ApolloClient, InMemoryCache, gql } from “apollo/client”;
